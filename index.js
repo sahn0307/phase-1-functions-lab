18afd42406da3ed2d0774564565038c653a20bc6
Sung-Jin Ahn <sahn0307@gmail.com>
2024-01-29 20:27:46 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const hqLocation = 42;
+    let distance;
+
+    if (location >= hqLocation) {
+        distance = location - hqLocation;
+    } else {
+        distance = hqLocation - location;
+    }
+
+    return distance;
+}
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264
+}
+function distanceTravelledInFeet(start, end) {
+    let distanceTravelled;
+    if (end >= start) {
+        distanceTravelled = end - start;
+    } else {
+        distanceTravelled = start - end;
+    }
+    
+    return distanceTravelled * 264;
+}
+
+function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
+console.log(distanceFromHqInBlocks(50));
+console.log(distanceFromHqInFeet(50));
+console.log(distanceTravelledInFeet(1, 3));
